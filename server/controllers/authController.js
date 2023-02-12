5b04670f9fca21c6ab1bdc0372cfd755a0271150
nazar2419 <nazarx095@gmail.com>
2023-02-12 15:05:33 +0200

    forgot password controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -122,9 +122,37 @@ const logout = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
 };
 
+const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    throw new CustomError.BadRequestError('Please provide valid email ');
+  }
+
+  const user = await User.findOne({ email });
+
+  if (user) {
+    const passwordToken = crypto.randomBytes(70).toString('hex');
+    // send email
+    const tenMinutes = 1000 * 60 * 10;
+    const passwordTokenExpirationDate = new Date(Date.now() + tenMinutes);
+
+    user.passwordToken = passwordToken;
+    user.passwordTokenExpirationDate = passwordTokenExpirationDate;
+    await user.save();
+  }
+  res.status(StatusCodes.OK).json({ msg: 'Please check your email for reset password link ' });
+  res.send('forgot password');
+};
+
+const resetPassword = async (req, res) => {
+  res.send('reset password');
+};
+
 module.exports = {
   register,
   login,
   logout,
   verifyEmail,
+  forgotPassword,
+  resetPassword,
 };
