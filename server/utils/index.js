3390bc2cc943d13641b75f05b5f26c3b06c36f89
nazar2419 <nazarx095@gmail.com>
2023-02-12 16:26:25 +0200

    send reset password email

--- server/utils/index.js
+++ server/utils/index.js
@@ -2,6 +2,7 @@ const { createJWT, isTokenValid, attachCookiesToResponse } = require('./jwt');
 const createTokenUser = require('./createTokenUser');
 const checkPermissions = require('./checkPermissions');
 const sendVerificationEmail = require('./sendVerificationEmail');
+const sendResetPasswordEmail = require('./sendResetPasswordEmail');
 module.exports = {
   createJWT,
   isTokenValid,
@@ -9,4 +10,5 @@ module.exports = {
   createTokenUser,
   checkPermissions,
   sendVerificationEmail,
+  sendResetPasswordEmail,
 };
